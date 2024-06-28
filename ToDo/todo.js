fc881175ff5416262b7bc9874a54d97216bce780
Madhup <madhupsharma22@gmail.com>
2024-06-29 01:28:42 +0530

    New Practice Project Files Added

---
+++ ToDo/todo.js
@@ -0,0 +1,11 @@
+// This is the ToDo JavaScript
+
+console.log('Js Actively Running');
+
+function addToDo(){
+    let inputElememt = document.querySelector('#task');
+    let toDoItem = inputElememt.value;
+    console.log(toDoItem);
+
+    
+}
